e2a943bb38a7f1a079883dfa8f98c508f5a3cf9b
TheodoreChuang <chuangtheodore@gmail.com>
2020-12-13 21:49:19 +1100

    feat(contract): Extend Dex contract, add wallet functionality.

--- Dex.sol
+++ Dex.sol
@@ -1,5 +1,7 @@
 pragma solidity 0.6.3;
 
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+
 contract Dex {
     struct Token {
         bytes32 ticker;
@@ -8,18 +10,56 @@ contract Dex {
 
     mapping(bytes32 => Token) public tokens;
     bytes32[] public tokenList;
+
+    // user => ticker => amount
+    mapping(address => mapping(bytes32 => uint256)) public tradeBalances;
+
     address public admin;
 
     constructor() public {
         admin = msg.sender;
     }
 
+    // Add new token to the Dex'ss token registry
     function addToken(bytes32 ticker, address tokenAddress)
         external
         onlyAdmin()
     {
         tokens[ticker] = Token(ticker, tokenAddress);
         tokenList.push(ticker);
+    }
+
+    // User to deposit token into the dex
+    function deposit(uint256 amount, bytes32 ticker)
+        external
+        tokenExist(ticker)
+    {
+        IERC20(tokens[ticker].tokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+
+        tradeBalances[msg.sender][ticker] += amount;
+    }
+
+    // User to withdraw their token from the dex
+    function withdraw(uint256 amount, bytes32 ticker)
+        external
+        tokenExist(ticker)
+    {
+        require(tradeBalances[msg.sender][ticker] >= amount, "balance too low");
+
+        tradeBalances[msg.sender][ticker] -= amount;
+        IERC20(tokens[ticker].tokenAddress).transfer(msg.sender, amount);
+    }
+
+    modifier tokenExist(bytes32 ticker) {
+        require(
+            tokens[ticker].tokenAddress != address(0),
+            "token does not exist"
+        );
+        _;
     }
 
     modifier onlyAdmin() {
