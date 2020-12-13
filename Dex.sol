e524a7edbfa8e7fcf713c9b4ec80bd38b181eac4
TheodoreChuang <chuangtheodore@gmail.com>
2020-12-13 17:18:35 +1100

    feat(contract): Add Dex contract, token registry

---
+++ Dex.sol
@@ -0,0 +1,29 @@
+pragma solidity 0.6.3;
+
+contract Dex {
+    struct Token {
+        bytes32 ticker;
+        address tokenAddress;
+    }
+
+    mapping(bytes32 => Token) public tokens;
+    bytes32[] public tokenList;
+    address public admin;
+
+    constructor() public {
+        admin = msg.sender;
+    }
+
+    function addToken(bytes32 ticker, address tokenAddress)
+        external
+        onlyAdmin()
+    {
+        tokens[ticker] = Token(ticker, tokenAddress);
+        tokenList.push(ticker);
+    }
+
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "only admin");
+        _;
+    }
+}
